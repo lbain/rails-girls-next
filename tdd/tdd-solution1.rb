1fc25439fb9f9d7468da0a857adc9b2b534d1c2b
Gregory McIntyre <gmcintyre@yourtutor.com.au>
2014-02-10 12:02:18 +1100

    Make solution code more consistent.

--- tdd/tdd-solution1.rb
+++ tdd/tdd-solution1.rb
@@ -1,21 +1,22 @@
+ROMAN_DIGIT_VALUES = [
+  [ "M", 1000 ],
+  ["CM",  900 ],
+  [ "D",  500 ],
+  [ "C",  100 ],
+  ["XC",   90 ],
+  [ "L",   50 ],
+  ["XL",   40 ],
+  [ "X",   10 ],
+  ["IX",    9 ],
+  [ "V",    5 ],
+  ["IV",    4 ],
+  [ "I",    1 ],
+]
+
 def roman(n)
-  roman_digit_values = [
-    [ "M", 1000 ],
-    ["CM",  900 ],
-    [ "D",  500 ],
-    [ "C",  100 ],
-    ["XC",   90 ],
-    [ "L",   50 ],
-    ["XL",   40 ],
-    [ "X",   10 ],
-    ["IX",    9 ],
-    [ "V",    5 ],
-    ["IV",    4 ],
-    [ "I",    1 ],
-  ]
   result = ""
   while n > 0
-    digit, value = roman_digit_values.find{|d, v| n >= v }
+    digit, value = ROMAN_DIGIT_VALUES.find{|d, v| n >= v }
     n -= value
     result += digit
   end
